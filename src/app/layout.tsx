d01478474e1ed305fc618a1c91c92ee469f84190
saif-appinventiv <saif.khan@appinventiv.com>
2026-08-10 15:51:11 +0530

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
-import favicon from "@/assets/images/favicon.ico";
 import "./globals.scss";
 
 export const metadata: Metadata = {
   title: "flynas",
   description: "flynas login and flight booking",
   icons: {
-    icon: favicon.src,
+    icon: "/favicon.ico",
   },
 };
 
